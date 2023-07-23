038195b1c096e1367479844574fd7f9c59087ff5
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-07-23 20:15:14 -0300

    creando funciones y enpoints principales de rutas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\RutaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,9 @@ Route::get("/usuarios", [UsuarioController::class, "Listar"]);
 Route::get("/usuarios/{id}", [UsuarioController::class, "ListarUno"]);
 Route::put("/usuarios/{id}", [UsuarioController::class, "Modificar"]);
 Route::delete("/usuarios/{id}", [UsuarioController::class, "Eliminar"]);
+
+Route::post("/rutas", [RutaController::class, "Crear"]);
+Route::get("/rutas", [RutaController::class, "Listar"]);
+Route::get("/rutas/{id}", [RutaController::class, "ListarUno"]);
+Route::put("/rutas/{id}", [RutaController::class, "Modificar"]);
+Route::delete("/rutas/{id}", [RutaController::class, "Eliminar"]);
