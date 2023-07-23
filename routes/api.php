035af5c6a4f0be43959b594d866b5a8a7d178010
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-07-23 19:25:34 -0300

    añadiendo principales endpoints de usuarios

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post("/usuarios", [UsuarioController::class, "Crear"]);
+Route::get("/usuarios", [UsuarioController::class, "Listar"]);
+Route::get("/usuarios/{id}", [UsuarioController::class, "ListarUno"]);
+Route::put("/usuarios/{id}", [UsuarioController::class, "Modificar"]);
+Route::delete("/usuarios/{id}", [UsuarioController::class, "Eliminar"]);
