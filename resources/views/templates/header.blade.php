7c6fe4ca30d5ee67ab540e5d504316f18a8170c9
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-09-08 15:13:03 -0300

    creando plantilla para el encabezado

---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Backoffice</title>
+</head>
+<body>
+    <header>
+        <div>
+            <a href="">Home</a>
+            <a href="">Usuarios</a>
+            <a href="">Camiones</a>
+            <a href="">Productos</a>
+            <a href="">Lotes</a>
+        </div>
+    </header>
+    <hr>
+
+    @yield('content')
+</body>
+</html>
