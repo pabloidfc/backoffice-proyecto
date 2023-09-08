fa5b32b3e39a5c8a3c80c554b13d52e387ac29f0
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-09-08 15:06:05 -0300

    comenzando a crear las rutas de web
    	- creando home view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get("/productos", function () {
+    return view("productos", ["productos"]);
+});
+
+// Route::post("/usuarios", [UsuarioController::class, "Crear"]);
+// Route::get("/usuarios", [UsuarioController::class, "Listar"]);
+// Route::get("/usuarios/{id}", [UsuarioController::class, "ListarUno"]);
+// Route::put("/usuarios/{id}", [UsuarioController::class, "Modificar"]);
+// Route::delete("/usuarios/{id}", [UsuarioController::class, "Eliminar"]);
+
+// Route::post("/rutas", [RutaController::class, "Crear"]);
+// Route::get("/rutas", [RutaController::class, "Listar"]);
+// Route::get("/rutas/{id}", [RutaController::class, "ListarUno"]);
+// Route::put("/rutas/{id}", [RutaController::class, "Modificar"]);
+// Route::delete("/rutas/{id}", [RutaController::class, "Eliminar"]);
+
+// Route::post("/camiones", [CamionController::class, "Crear"]);
+// Route::get("/camiones", [CamionController::class, "Listar"]);
+// Route::get("/camiones/{id}", [CamionController::class, "ListarUno"]);
+// Route::put("/camiones/{id}", [CamionController::class, "Modificar"]);
+// Route::delete("/camiones/{id}", [CamionController::class, "Eliminar"]);
+
+// Route::post("/almacenes", [AlmacenController::class, "Crear"]);
+// Route::get("/almacenes", [AlmacenController::class, "Listar"]);
+// Route::get("/almacenes/{id}", [AlmacenController::class, "ListarUno"]);
+// Route::put("/almacenes/{id}", [AlmacenController::class, "Modificar"]);
+// Route::delete("/almacenes/{id}", [AlmacenController::class, "Eliminar"]);
