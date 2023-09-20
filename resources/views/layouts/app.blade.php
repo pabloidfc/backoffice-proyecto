d9fe4994025f32aed77d08bcc0a1be702ffd6e96
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-09-20 19:43:46 -0300

    creando SessionController, permite logear el usuario admin
    	- agregando Bootstrap a las vistas
    	- protegiendo rutas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SERSolutions - @yield("title")</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
     <style>
         a {
             text-decoration: none;
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\AlmacenController;
 use App\Http\Controllers\LoteController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,53 +19,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-}) -> name("home");
+}) 
+-> name("home")
+-> middleware("auth");
 
-Route::controller(ProductoController::class) -> group(function () {
-    Route::get("/producto", "index") -> name("producto.index");
-    Route::get("/producto/crear", "create") -> name("producto.create");
-    Route::get("/producto/{id}", "show") -> name("producto.show");
-    Route::get("/producto/{id}/editar", "edit") -> name("producto.edit");
-    Route::put("/producto/{id}", "update") -> name("producto.update");
-    Route::delete("/producto/{id}", "destroy") -> name("producto.destroy");
-    Route::post("producto/", "store") -> name("producto.store");
+Route::controller(SessionController::class) -> group(function () {
+    Route::get("/login",  "login") -> name("login");
+    Route::post("/login", "store") -> name("login");
 });
 
-Route::controller(LoteController::class) -> group(function () {
-    Route::get("/lote", "index") -> name("lote.index");
-    Route::get("/lote/{id}", "show") -> name("lote.show");
+Route::middleware("auth")->group(function () {
+    Route::controller(ProductoController::class) -> group(function () {
+        Route::get("/producto", "index") -> name("producto.index");
+        Route::get("/producto/crear", "create") -> name("producto.create");
+        Route::get("/producto/{id}", "show") -> name("producto.show");
+        Route::get("/producto/{id}/editar", "edit") -> name("producto.edit");
+        Route::put("/producto/{id}", "update") -> name("producto.update");
+        Route::delete("/producto/{id}", "destroy") -> name("producto.destroy");
+        Route::post("producto/", "store") -> name("producto.store");
+    });
+    
+    Route::controller(LoteController::class) -> group(function () {
+        Route::get("/lote", "index") -> name("lote.index");
+        Route::get("/lote/{id}", "show") -> name("lote.show");
+    });
+    
+    Route::controller(AlmacenController::class) -> group(function () {
+        Route::get("/almacen", "index")            -> name("almacen.index");
+        Route::get("/almacen/crear", "create")     -> name("almacen.create");
+        Route::delete("/almacen/{id}", "destroy")  -> name("almacen.destroy");
+        Route::put("/almacen/{id}", "update")      -> name("almacen.update");
+        Route::get("/almacen/{id}/editar", "edit") -> name("almacen.edit");
+        Route::get("/almacen/{id}", "show")        -> name("almacen.show");
+        Route::post("/almacen", "store")           -> name("almacen.store");
+    });
 });
-
-Route::controller(AlmacenController::class) -> group(function () {
-    Route::get("/almacen", "index")            -> name("almacen.index");
-    Route::get("/almacen/crear", "create")     -> name("almacen.create");
-    Route::delete("/almacen/{id}", "destroy")  -> name("almacen.destroy");
-    Route::put("/almacen/{id}", "update")      -> name("almacen.update");
-    Route::get("/almacen/{id}/editar", "edit") -> name("almacen.edit");
-    Route::get("/almacen/{id}", "show")        -> name("almacen.show");
-    Route::post("/almacen", "store")           -> name("almacen.store");
-});
-
-// Route::post("/usuarios", [UsuarioController::class, "Crear"]);
-// Route::get("/usuarios", [UsuarioController::class, "Listar"]);
-// Route::get("/usuarios/{id}", [UsuarioController::class, "ListarUno"]);
-// Route::put("/usuarios/{id}", [UsuarioController::class, "Modificar"]);
-// Route::delete("/usuarios/{id}", [UsuarioController::class, "Eliminar"]);
-
-// Route::post("/rutas", [RutaController::class, "Crear"]);
-// Route::get("/rutas", [RutaController::class, "Listar"]);
-// Route::get("/rutas/{id}", [RutaController::class, "ListarUno"]);
-// Route::put("/rutas/{id}", [RutaController::class, "Modificar"]);
-// Route::delete("/rutas/{id}", [RutaController::class, "Eliminar"]);
-
-// Route::post("/camiones", [CamionController::class, "Crear"]);
-// Route::get("/camiones", [CamionController::class, "Listar"]);
-// Route::get("/camiones/{id}", [CamionController::class, "ListarUno"]);
-// Route::put("/camiones/{id}", [CamionController::class, "Modificar"]);
-// Route::delete("/camiones/{id}", [CamionController::class, "Eliminar"]);
-
-// Route::post("/almacenes", [AlmacenController::class, "Crear"]);
-// Route::get("/almacenes", [AlmacenController::class, "Listar"]);
-// Route::get("/almacenes/{id}", [AlmacenController::class, "ListarUno"]);
-// Route::put("/almacenes/{id}", [AlmacenController::class, "Modificar"]);
-// Route::delete("/almacenes/{id}", [AlmacenController::class, "Eliminar"]);
