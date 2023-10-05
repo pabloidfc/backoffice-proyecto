c1b8f90c51645a671fa55eea1de50be941ba9513
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-10-05 17:45:48 -0300

    comenzando a cambiar vista de creacion de usuarios

--- resources/views/usuario/create.blade.php
+++ resources/views/usuario/create.blade.php
@@ -3,220 +3,280 @@
 @section('title', "Usuarios")
     
 @section("content")
-<h1>Crear Nuevo Usuario</h1>
-<p>Los campos con (*) son obligatorios!</p>
-<form 
-    action="{{ route("usuario.store") }}"
-    method="POST"
-    style="display: inline-block"
->
+<div class="container">
+    <h1 class="mb-0">Crear nuevo usuario</h1>
+    <p>Los campos con (*) son obligatorios!</p>
+    <div class="col">
+    <form 
+        action="{{ route("usuario.store") }}"
+        method="POST"
+        class="row"
+    >
     @csrf
-    <label>
-        CI sin puntos ni guíon* <br>
-        <input name="ci" type="text" value="{{old("ci")}}">
-        @error('ci')
-            <br>
-            <small style="color: red">{{ $message }}</small>   
-        @enderror
-    </label>
-    <br>
-    <label>
-        Nombre* <br>
-        <input name="nombre" type="text" value="{{old("nombre")}}">
-        @error('nombre')
-            <br>
-            <small style="color: red">{{ $message }}</small>   
-        @enderror
-    </label>
-    <br>
-    <label>
-        Segundo nombre <br>
-        <input name="nombre2" type="text" value="{{old("nombre2")}}">
-        @error('nombre2')
-            <br>
-            <small style="color: red">{{ $message }}</small>   
-        @enderror
-    </label>
 
-    <br>
-
-    <label>
-        Apellido* <br>
-        <input name="apellido" type="text" value="{{old("apellido")}}">
-        @error('apellido')
-            <br>
-            <small style="color: red">{{ $message }}</small>   
-        @enderror
-    </label>
-    <br>
-    <label>
-        Segundo apellido* <br>
-        <input name="apellido2" type="text" value="{{old("apellido2")}}">
-        @error('apellido2')
-            <br>
-            <small style="color: red">{{ $message }}</small>   
-        @enderror
-    </label>
-
-    <br>
-
-    <label>
-        Email* <br>
-        <input name="email" type="email" value="{{old("email")}}">
-        @error('email')
-            <br>
-            <small style="color: red">{{ $message }}</small>   
-         @enderror
-    </label>
-    <br>
-    <label>
-        Teléfono* <br>
-        <input name="telefono" type="number" value="{{old("telefono")}}">
-        @error('telefono')
-            <br>
-            <small style="color: red">{{ $message }}</small>   
-         @enderror
-    </label>
-    <br>
-    <label>
-        Contraseña* <br>
-        <input name="password" type="password" value="{{old("password")}}">
-        @error('password')
-            <br>
-            <small style="color: red">{{ $message }}</small>   
-         @enderror
-    </label>
-    <br>
-    <label>
-        Confirmar contraseña* <br>
-        <input name="password_confirmation" type="password" value="{{old("password_confirmation")}}">
-        @error('password_confirmation')
-            <br>
-            <small style="color: red">{{ $message }}</small>   
-         @enderror
-    </label>
-
-    <div>
-        <label>
-            Departamento* <br>
-            <input name="departamento" type="text" value="{{old('departamento')}}">
-        </label>
-        @error("departamento")
-            <br>
-            <small style="color: red">{{ $message }}</small> 
-        @enderror
-        <br>
-        <label>
-            Calle* <br>
-            <input name="calle" type="text" value="{{old('calle')}}">
-        </label>
-        @error("calle")
-            <br>
-            <small style="color: red">{{ $message }}</small> 
-        @enderror
-        <br>
-        <label>
-            N° de puerta* <br>
-            <input name="nro_de_puerta" type="number" value="{{old('nro_de_puerta')}}">
-        </label>
-        @error("nro_de_puerta")
-            <br>
-            <small style="color: red">{{ $message }}</small> 
-        @enderror
-        <br>
-        <label>
-            Esquina <br>
-            <input name="esquina" type="text" value="{{old('esquina')}}">
-            @error("esquina")
-                <br>
-                <small style="color: red">{{ $message }}</small> 
-            @enderror
-        </label>
-        <br>
-        <label>
-            Coordenada <br>
-            <input name="coordenada" type="text" value="{{old('coordenada')}}">
-            @error("esquina")
-                <br>
-                <small style="color: red">{{ $message }}</small> 
-            @enderror
-        </label>
-        <br>
-    </div>
-
-    <fieldset>
-        <legend>Selecciona un tipo*</legend>
-        <div>
-            <label>
-                <input 
-                    type="radio"
-                    name="permisos" 
-                    id="input-funcionario"
-                    value="Funcionario" 
-                >
-                Funcionario
-            </label>
-            <label>
-                <input 
-                    type="radio"
-                    name="permisos" 
-                    value="Transportista" 
-                >
-                Transportista
-            </label>
-            @error("permisos")
-                <br>
-                <small style="color: red">{{ $message }}</small>   
-            @enderror
-            @error("tipo")
-                <br>
-                <small style="color: red">{{ $message }}</small>   
-            @enderror
-            @error('almacen_id')
-                <br>
-                <small style="color: red">{{ $message }}</small>   
-            @enderror
-            @error('empresa_id')
-                <br>
-                <small style="color: red">{{ $message }}</small>   
+        <div class="form-group col-auto">
+            <label for="ci" class="form-label">CI sin puntos ni guíon*</label>
+            <input 
+            id="ci" 
+            class="form-control"
+            name="ci" 
+            type="text" 
+            value="{{old("ci")}}">
+            @error("ci")
+                <small class="form-text text-danger">{{ $message }}</small>   
             @enderror
         </div>
-        <br>
-        <div id="radio-tipo-contenedor" class="d-none">
-            <label>
+        
+        <div class="form-group row">
+            <div class="col-auto">
+                <label for="nombre" class="form-label">Nombre*</label>
                 <input 
-                    type="radio"
-                    name="tipo" 
-                    value="Propio" 
+                id="nombre" 
+                class="form-control"
+                name="nombre" 
+                type="text" 
+                value="{{old("nombre")}}">
+                @error("nombre")
+                    <small class="form-text text-danger">{{ $message }}</small>   
+                @enderror
+            </div>
+            <div class="col-auto">
+                <label for="nombre2" class="form-label">Segundo nombre</label>
+                <input 
+                id="nombre2" 
+                class="form-control"
+                name="nombre2" 
+                type="text" 
+                value="{{old("nombre2")}}">
+                @error("nombre2")
+                    <small class="form-text text-danger">{{ $message }}</small>   
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-auto">
+                <label for="apellido" class="form-label">Apellido*</label>
+                <input 
+                id="apellido" 
+                class="form-control"
+                name="apellido" 
+                type="text" 
+                value="{{old("apellido")}}">
+                @error("apellido")
+                    <small class="form-text text-danger">{{ $message }}</small>   
+                @enderror
+            </div>
+            <div class="col-auto">
+                <label for="apellido2" class="form-label">Segundo apellido*</label>
+                <input 
+                id="apellido2" 
+                class="form-control"
+                name="apellido2" 
+                type="text" 
+                value="{{old("apellido2")}}">
+                @error("apellido2")
+                    <small class="form-text text-danger">{{ $message }}</small>   
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-auto">
+                <label for="email" class="form-label">Email*</label>
+                <input 
+                id="email" 
+                class="form-control"
+                name="email" 
+                type="email" 
+                value="{{old("email")}}">
+                @error("email")
+                    <small class="form-text text-danger">{{ $message }}</small>   
+                @enderror
+            </div>
+            <div class="col-auto">
+                <label for="telefono" class="form-label">Teléfono*</label>
+                <input 
+                id="telefono" 
+                class="form-control"
+                name="telefono" 
+                type="text" 
+                value="{{old("telefono")}}">
+                @error("telefono")
+                    <small class="form-text text-danger">{{ $message }}</small>   
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-auto">
+                <label for="password" class="form-label">Contraseña*</label>
+                <input 
+                id="password"
+                class="form-control"
+                name="password" 
+                type="password" 
                 >
-                Propio
+                @error("password")
+                    <small class="form-text text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+            <div class="col-auto">
+                <label for="password_confirmation" class="form-label">Confirmar Contraseña*</label>
+                <input 
+                id="password_confirmation"
+                class="form-control"
+                name="password_confirmation" 
+                type="password" 
+                >
+                @error('password_confirmation')
+                    <small class="form-text text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-auto">
+                <label for="departamento" class="form-label">Departamento*</label>
+                <input 
+                id="departamento"
+                class="form-control"
+                name="departamento" 
+                type="text" 
+                >
+                @error("departamento")
+                    <small class="form-text text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+            <div class="col-auto">
+                <label for="calle" class="form-label">Calle*</label>
+                <input 
+                id="calle"
+                class="form-control"
+                name="calle" 
+                type="text" 
+                >
+                @error("calle")
+                    <small class="form-text text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+            <div class="col-auto">
+                <label for="nro_de_puerta" class="form-label">Número de puerta*</label>
+                <input 
+                id="nro_de_puerta"
+                class="form-control"
+                name="nro_de_puerta" 
+                type="number" 
+                >
+                @error("nro_de_puerta")
+                    <small class="form-text text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-auto">
+                <label for="esquina" class="form-label">Esquina</label>
+                <input 
+                id="esquina"
+                class="form-control"
+                name="esquina" 
+                type="text" 
+                >
+                @error("esquina")
+                    <small class="form-text text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+            <div class="col-auto">
+                <label for="coordenada" class="form-label">Coordenada</label>
+                <input 
+                id="coordenada"
+                class="form-control"
+                name="coordenada" 
+                type="text" 
+                >
+                @error("coordenada")
+                    <small class="form-text text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+        </div>
+
+
+        <fieldset>
+            <legend>Selecciona un tipo*</legend>
+            <div>
+                <label>
+                    <input 
+                        type="radio"
+                        name="permisos" 
+                        id="input-funcionario"
+                        value="Funcionario" 
+                    >
+                    Funcionario
+                </label>
+                <label>
+                    <input 
+                        type="radio"
+                        name="permisos" 
+                        value="Transportista" 
+                    >
+                    Transportista
+                </label>
+                @error("permisos")
+                    <br>
+                    <small style="color: red">{{ $message }}</small>   
+                @enderror
+                @error("tipo")
+                    <br>
+                    <small style="color: red">{{ $message }}</small>   
+                @enderror
+                @error('almacen_id')
+                    <br>
+                    <small style="color: red">{{ $message }}</small>   
+                @enderror
+                @error('empresa_id')
+                    <br>
+                    <small style="color: red">{{ $message }}</small>   
+                @enderror
+            </div>
+            <br>
+            <div id="radio-tipo-contenedor" class="d-none">
+                <label>
+                    <input 
+                        type="radio"
+                        name="tipo" 
+                        value="Propio" 
+                    >
+                    Propio
+                </label>
+                <label>
+                    <input 
+                        type="radio"
+                        name="tipo" 
+                        value="De terceros" 
+                    >
+                    De terceros
+                </label>
+            </div>
+        </fieldset>
+
+        <div id="almacen-empresa-contenedor" class="d-none">
+            <label>
+                Almacen de trabajo* <br>
+                <input name="almacen_id" type="number" id="input-almacen">
             </label>
-            <label>
-                <input 
-                    type="radio"
-                    name="tipo" 
-                    value="De terceros" 
-                >
-                De terceros
+        
+            <label id="empresa-perteneciente" class="d-none">
+                Empresa a la que pertenece* <br>
+                <input name="empresa_id" type="number" id="input-empresa">
             </label>
         </div>
-    </fieldset>
 
-    <div id="almacen-empresa-contenedor" class="d-none">
-        <label>
-            Almacen de trabajo* <br>
-            <input name="almacen_id" type="number" id="input-almacen">
-        </label>
-     
-        <label id="empresa-perteneciente" class="d-none">
-            Empresa a la que pertenece* <br>
-            <input name="empresa_id" type="number" id="input-empresa">
-        </label>
+        <div class="col-auto">
+            <button class="btn btn-primary" type="submit">Enviar</button>
+        </div>
+    </form>
     </div>
-
-    <br>
-
-    <button type="submit">Enviar</button>
-</form>
+</div>
 <script src="/js/usuario/usuario.create.js"></script>
 @endsection
